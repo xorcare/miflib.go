a972b262a344850a92ba4c29b3b58f59c065616a
Vasiliy Vasilyuk <xorcare@gmail.com>
2020-06-20 21:19:13 +0300

    docs: update LICENSE

--- internal/book/cover.go
+++ internal/book/cover.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2020 Vasiliy Vasilyuk All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package book
 
 // Cover address on the cover of the book.
